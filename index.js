967469682075d6201672cf6bd8d91f740d2242c3
Sushmitha Rasmani <sushmitharasmani2700@gmail.com>
2019-12-19 11:18:06 +0530

    created web server

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+const express = require('express')
+const app = express()
+constno port = 3000
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
